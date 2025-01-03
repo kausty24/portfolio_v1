d99d9623d32f6c8bebfe1d982d7f2503a4256c10
kausty24 <kaustubhpathak24@gmail.com>
2025-01-03 13:50:54 +0530

    Added initial changes for portfolio

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const carousel = document.querySelector('.carousel');
+let index = 0;
+
+function moveCarousel(direction) {
+  const items = document.querySelectorAll('.carousel-item');
+  index += direction;
+
+  if (index < 0) index = items.length - 1;
+  if (index >= items.length) index = 0;
+
+  carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
